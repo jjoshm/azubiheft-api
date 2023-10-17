4ef228ecfe435ae35ff420e72fef0b52d664380c
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2023-10-17 14:49:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="azubiheftApi",
-    version="0.0.3",
+    version="0.0.4",
     author="Leon Kohlhaussen",
     author_email="kohli.leon@gmail.com",
     description="Azubiheft.de custom api",
